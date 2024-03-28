4813b5ce80de7866b1a14df9080a79956b1d5738
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 06:54:39 +0800

    ArchLinux

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 pkg upgrade
+
 pkg install git
 pkg install root-repo
 pkg install x11-repo
+pkg install neofetch
+
+# installs programming languages
+pkg install python
+pkg install clang
+mkdir ~/apps
+
 termux-setup-storage
+
+pkg install proot-distro
+
+proot-distro install archlinux
+
+proot-distro login archlinux
