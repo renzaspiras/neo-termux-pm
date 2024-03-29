90e7e103e7fb7168c5832a0add15555f642ff2a6
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 23:53:11 +0800

    catching...

--- termux/archlinux.sh
+++ termux/archlinux.sh
@@ -6,6 +6,6 @@ proot-distro login archlinux -- pacman -S doas --noconfirm
 proot-distro login archlinux -- pacman -S neovim --noconfirm
 proot-distro login archlinux -- chsh -s /bin/zsh
 
-meow=$(cat /termux/.zshrc)
+meow=$(cat ~/hello/termux/.zshrc)
 proot-distro login archlinux -- touch .zshrc
-# proot-distro login archlinux -- echo "$meow" > ~/.zshrc
+proot-distro login archlinux -- echo "$meow" > ~/.zshrc
