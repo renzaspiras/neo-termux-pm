4d5ea9dd414f4b7b929bed0a880968946343e7b8
renzaspiras <aspirasrenz@gmail.com>
2024-03-30 06:43:03 +0800

    patch update

--- patch/patch.sh
+++ patch/patch.sh
@@ -1,2 +1,5 @@
 clear
-echo "Nothing new yet....";
+
+# Auto Fixes the .zshrc without reinstalling the entire system...
+rm ~/.zshrc
+wget https://raw.githubusercontent.com/renzaspiras/arch-termux/main/termux/.zshrc ~/.zshrc
