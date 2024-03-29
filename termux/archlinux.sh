80cee35ee3f7b3583b25017479570d339832b712
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 22:43:52 +0800

    zsh

--- termux/archlinux.sh
+++ termux/archlinux.sh
@@ -4,3 +4,4 @@ proot-distro login archlinux -- pacman -S neofetch --noconfirm
 proot-distro login archlinux -- pacman -S zsh --noconfirm
 proot-distro login archlinux -- pacman -S doas --noconfirm
 proot-distro login archlinux -- pacman -S neovim --noconfirm
+proot-distro login archlinux -- chsh -s /bin/zsh
