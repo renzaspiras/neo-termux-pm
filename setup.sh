e21f993319c2446ccd09a88e9d18faac62555c1a
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 15:41:38 +0800

    \

--- setup.sh
+++ setup.sh
@@ -1,21 +1,13 @@
-# Craft Matrix
-
-pkg upgrade;
-
-pkg install git;
-pkg install root-repo;
-pkg install x11-repo;
-pkg install neofetch;
-pkg install proot-distro;
-pkg install tmux;
-
-# installs programming languages
-pkg install python;
-pkg install clang;
-mkdir ~/apps;
-
-termux-setup-storage;
-
-proot-distro install archlinux;
-
+pkg upgrade; \
+pkg install git; \
+pkg install root-repo; \
+pkg install x11-repo; \
+pkg install neofetch; \
+pkg install proot-distro; \
+pkg install tmux; \
+pkg install python; \
+pkg install clang; \
+mkdir ~/apps; \
+termux-setup-storage; \
+proot-distro install archlinux; \
 proot-distro login archlinux; #-- any bash commands;
