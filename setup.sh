93673860a17a233258cb95ba8ddd5fba80fe2ae5
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 15:46:52 +0800

    with package

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 pkg-update="https://raw.githubusercontent.com/renzaspiras/arch-termux/main/pkg-upgrade.sh"
-
+pkg="https://raw.githubusercontent.com/renzaspiras/arch-termux/main/package.sh"
 # updates the package
 curl -fsSL $pkg-update | sh
+
+#installs the packages
+curl -fsSL $pkg |sh
 
 mkdir ~/apps; \
 termux-setup-storage; \
