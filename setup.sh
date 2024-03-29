dbea141a19d2415de78983872f101dcd407cfdf4
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 15:54:04 +0800

    clean

--- setup.sh
+++ setup.sh
@@ -7,8 +7,3 @@ curl -fsSL $pkg-update | sh
 
 #installs the packages
 curl -fsSL $pkg |sh
-
-mkdir ~/apps; \
-termux-setup-storage; \
-proot-distro install archlinux; \
-proot-distro login archlinux; #-- any bash commands;
