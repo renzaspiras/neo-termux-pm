14a0fdab72c9c8bc386ada9db3bd7c5e50ea6424
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 23:13:50 +0800

    kalinified

--- termux/archlinux.sh
+++ termux/archlinux.sh
@@ -5,3 +5,6 @@ proot-distro login archlinux -- pacman -S zsh --noconfirm
 proot-distro login archlinux -- pacman -S doas --noconfirm
 proot-distro login archlinux -- pacman -S neovim --noconfirm
 proot-distro login archlinux -- chsh -s /bin/zsh
+
+meow=$(cat .zshrc)
+proot-distro login archlinux -- echo "$meow" > ~/.zshrc
