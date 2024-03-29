993082a6e04a2870a43905feb239d31611b0a1a5
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 15:52:00 +0800

    rebootify

--- package.sh
+++ package.sh
@@ -1,10 +1,1 @@
 echo "install package"
-
-# pkg install git;
-# pkg install root-repo;
-# pkg install x11-repo;
-# pkg install neofetch;
-# pkg install proot-distro;
-# pkg install tmux;
-# pkg install python;
-# pkg install clang;
--- pkg-upgrade.sh
+++ pkg-upgrade.sh
@@ -1,3 +1,1 @@
 echo "update"
-
-# pkg upgrade
