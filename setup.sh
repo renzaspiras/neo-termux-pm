63fc1a6a31d62a06e554cfd911a59429fdd43289
renzaspiras <aspirasrenz@gmail.com>
2024-03-29 19:38:19 +0800

    ArchLinux

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 yes | bash ./termux/package.sh
 
 bash ./termux/archlinux.sh
-
+bash ./archlinux/update.sh
 bash ./termux/config.sh
--- termux/archlinux.sh
+++ termux/archlinux.sh
@@ -1,3 +1,5 @@
 proot-distro install archlinux
-
+clear
 proot-distro login archlinux -- pacman -S neofetch --noconfirm
+proot-distro login archlinux -- pacman -S zsh --noconfirm
+proot-distro login archlinux -- pacman -S bpytop --noconfirm
