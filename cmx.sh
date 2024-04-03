c8a3c2672b1df2fd1f6c35551e977b4a76497443
Renz Cruz Aspiras <89981046+renzaspiras@users.noreply.github.com>
2024-04-04 02:53:22 +0800

    Update cmx.sh

--- cmx.sh
+++ cmx.sh
@@ -1,3 +1,4 @@
 yes | pkg upgrade && yes | pkg install git && git clone https://github.com/renzaspiras/arch-termux.git ~/hello && cd ~/hello && bash setup.sh
 
-mkdir storage/shared/TERMUX
+cd storage/shared/
+mkdir TERMUX
