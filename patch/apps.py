afa25173509fbb42b88d8031865546c9597338ad
renzaspiras <aspirasrenz@gmail.com>
2024-04-06 17:57:39 +0800

    re write python

--- patch/apps.py
+++ patch/apps.py
@@ -1,11 +1,7 @@
 import os
 
 # Define the directory containing the scripts
-script_dir = os.path.expanduser("~/apps")
 
 # Iterate over subdirectories and run up.sh scripts
-for root, dirs, files in os.walk(script_dir):
-    if "up.sh" in files:
-        script_path = os.path.join(root, "up.sh")
-        print(f"Running script: {script_path}")
-        os.system(f"bash {script_path}")
+apps = os.listdir("~/apps")
+print(apps)
