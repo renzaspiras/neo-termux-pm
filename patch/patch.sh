b23f0851a53981e9fc5b53cb5532a2d735d2858f
renzaspiras <aspirasrenz@gmail.com>
2024-04-06 13:49:43 +0800

    updated patch

--- patch/patch.sh
+++ patch/patch.sh
@@ -18,37 +18,6 @@ fi
 # Get a list of directories in ~/apps
 directories=$(find "$APPS_DIR" -maxdepth 1 -mindepth 1 -type d)
 
-# Iterate over each directory
-for dir in $directories; do
-    # Extract directory name
-    dir_name=$(basename "$dir")
-    
-    # Check if upcm.sh exists in the directory
-    if [ -f "$dir/upcm.sh" ]; then
-        echo "Running upcm.sh in directory $dir_name"
-        # Run upcm.sh
-        bash "$dir/upcm.sh"
-    else
-        echo "upcm.sh not found in directory $dir_name"
-    fi
-done
-
-# Check if cmake is installed
-if ! command -v cmake &> /dev/null; then
-    echo "cmake is not installed. Installing..."
-    pacman -S cmake --noconfirm
-else
-    echo "cmake is already installed."
-fi
-
-# Check if jsoncpp-doc is installed
-if ! pacman -Qs jsoncpp-doc &> /dev/null; then
-    echo "jsoncpp-doc is not installed. Installing..."
-    pacman -S jsoncpp-doc --noconfirm
-else
-    echo "jsoncpp-doc is already installed."
-fi
-
 
 git clone https://github.com/renzaspiras/arch-termux.git ~/patch
 cd ~/patch/patch/
--- setup.sh
+++ setup.sh
@@ -12,6 +12,9 @@ yes | bash ~/hello/termux/package.sh
 
 yes | termux-setup-storage
 
-yes | bash ~/hello/patch/layer0.sh
+# yes | bash ~/hello/patch/layer0.sh
+echo 'alias update="wget https://raw.githubusercontent.com/renzaspiras/arch-termux/main/patch/patch.sh -O /path/to/save/patch.sh && bash /path/to/save/patch.sh"' >> ~/.zshrc
+source ~/.zshrc
+
 
 bash ~/hello/termux/config.sh 
