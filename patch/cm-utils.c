2bd80b39b70ca7d10f083bff11d4cb0415fa3f7b
renzaspiras <aspirasrenz@gmail.com>
2024-04-07 07:26:11 +0800

    fixed architecture....

--- patch/cm-utils.c
+++ patch/cm-utils.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
         else if (strcmp(argv[1], "add") == 0) {
             char command[1000];
             system("rm -rf ~/temp");
-            snprintf(command, sizeof(command), "git clone %s ~/temp && bash ~/temp/setup.sh TERMUX", argv[2]);
+            snprintf(command, sizeof(command), "git clone %s ~/temp && bash ~/temp/setup.sh PTERMUX", argv[2]);
             int status = system(command);
             if (status != 0) {
                 fprintf(stderr, "Error executing command\n");
