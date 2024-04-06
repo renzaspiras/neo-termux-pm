0f7144e3415fbf683b512b3ffe8d45125fce1c4d
Renz Cruz Aspiras <89981046+renzaspiras@users.noreply.github.com>
2024-04-06 01:45:05 +0000

    zsh

--- termux/config.sh
+++ termux/config.sh
@@ -3,6 +3,13 @@ yes | rm -r ~/hello
 cd ~/
 clear
 
+git clone https://github.com/Sohil876/Termux-zsh.git zsh
+cd zsh
+yes | bash setup.sh
+cd ~/
+
+
 touch storage/shared/TERMUX/TOKEN
 
 echo "Done Installing. You May Now Type [Exit] and [Enter]"
+exit
