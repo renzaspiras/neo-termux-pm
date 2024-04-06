1de4d1455185b86365a6109381b0df7c4ed9fce6
Renz Cruz Aspiras <89981046+renzaspiras@users.noreply.github.com>
2024-04-06 01:53:53 +0000

    unuse manager

--- termux/config.sh
+++ termux/config.sh
@@ -6,10 +6,11 @@ clear
 git clone https://github.com/Sohil876/Termux-zsh.git zsh
 cd zsh
 yes | bash setup.sh
+yes | rm -r ~/zsh
 cd ~/
+
 
 
 touch storage/shared/TERMUX/TOKEN
 
-echo "Done Installing. You May Now Type [Exit] and [Enter]"
 exit
