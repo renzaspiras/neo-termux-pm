83b4b763cf03847416f57845c2079923162b593f
renzaspiras <aspirasrenz@gmail.com>
2024-04-06 18:04:40 +0800

    exec

--- patch/apps.py
+++ patch/apps.py
@@ -6,4 +6,4 @@ apps_dir = os.path.expanduser("~/apps")
 # Iterate over subdirectories and run up.sh scripts
 apps = os.listdir(apps_dir)
 for j in apps:
-  print("bash ~/apps/" + j + "/up.sh")
+  os.system("bash ~/apps/" + j + "/up.sh")
