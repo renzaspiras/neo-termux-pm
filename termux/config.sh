d4034ec39b3e888cdac9fe575638b2f117288ccc
renzaspiras <aspirasrenz@gmail.com>
2024-04-06 14:32:27 +0800

    re routed

--- termux/config.sh
+++ termux/config.sh
@@ -1,11 +1,12 @@
 cp ~/hello/termux/.bashrc ~/.bashrc
-yes | rm -r ~/hello
-cd ~/
+
 clear
 
 mv ~/hello/termux/.zshrc ~/.zshrc
 wget https://raw.githubusercontent.com/Sohil876/Termux-zsh/master/OhMyZsh/zshrc -O .omz.sh
 
 touch storage/shared/TERMUX/TOKEN
+yes | rm -r ~/hello
+cd ~/
 
 echo "[DONE]"
