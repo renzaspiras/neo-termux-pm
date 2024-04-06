fcd72a84dcde4e759853329d60d96717587eee60
renzaspiras <aspirasrenz@gmail.com>
2024-04-06 14:57:37 +0800

    updated package manager

--- patch/cm-utils.c
+++ patch/cm-utils.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>  // for system()
 
-int main(){
-  printf("x update");
-  return 0;
+int main(int argc, char *argv[]) {
+    // Check if there are command-line arguments
+    if (argc > 1) {
+        // Check if the argument is "update"
+        if (strcmp(argv[1], "update") == 0) {
+            printf("Executing update command...\n");
+            // Execute the system command named "update"
+            system("update");
+        } else {
+            printf("Invalid argument\n");
+        }
+    } else {
+        printf("No argument provided\n");
+    }
+    
+    return 0;
 }
