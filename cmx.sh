a2742cffd42129e911338c5582225592327ae49e
Renz Cruz Aspiras <89981046+renzaspiras@users.noreply.github.com>
2024-04-06 08:05:00 +0800

    Plain

--- cmx.sh
+++ cmx.sh
@@ -1,4 +1,1 @@
 yes | pkg upgrade && yes | pkg install git && git clone https://github.com/renzaspiras/arch-termux.git ~/hello && cd ~/hello && bash setup.sh
-
-cd storage/shared/TERMUX
-touch TOKEN
