8a1d427f6af350844d49d046bddea2dcae76e0db
renzaspiras <aspirasrenz@gmail.com>
2024-04-06 18:00:05 +0800

    retro

--- patch/apps.py
+++ patch/apps.py
@@ -1,7 +1,8 @@
 import os
 
 # Define the directory containing the scripts
+apps_dir = os.path.expanduser("~/apps")
 
 # Iterate over subdirectories and run up.sh scripts
-apps = os.listdir("~/apps")
+apps = os.listdir(apps_dir)
 print(apps)
