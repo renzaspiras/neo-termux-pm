f8ade2f4d306b613fd8dbc2d0bd0cf01173b8d61
Renz Cruz Aspiras <89981046+renzaspiras@users.noreply.github.com>
2024-04-07 20:46:54 +0800

    Update cmx.sh

--- cmx.sh
+++ cmx.sh
@@ -1,2 +1,2 @@
 # The Installer of Android Termux
-yes | pkg upgrade && yes | pkg install git && git clone https://github.com/renzaspiras/arch-termux.git ~/hello && cd ~/hello && bash setup.sh
+yes | pkg upgrade && yes | pkg install git && git clone https://github.com/renzaspiras/neo-termux-archlinux ~/hello && cd ~/hello && bash setup.sh
