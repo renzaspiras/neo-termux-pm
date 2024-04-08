be0cf0f61ac7c86ec2a0125360ed2486b246235a
renzaspiras <aspirasrenz@gmail.com>
2024-04-08 09:08:13 +0800

    token

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# Deletes the Default Messages on the startup of the termux
+# Deletes the Default Messages on the startup of the TERMUX
 cd ..
 cd usr/etc
 rm -rf motd
@@ -9,43 +9,47 @@ yes | termux-setup-storage
 
 # File System
 # Main directory for the filesystem
-mkdir storage/shared/termux/filesystem  
+mkdir storage/shared/TERMUX/filesystem  
 
 # Binaries directory for executable files
-mkdir storage/shared/termux/filesystem/bin  
+mkdir storage/shared/TERMUX/filesystem/bin  
 
 # Directory for installed applications
-mkdir storage/shared/termux/filesystem/apps  
+mkdir storage/shared/TERMUX/filesystem/apps  
 
 # Directory for scripts or applications to run on system startup
-mkdir storage/shared/termux/filesystem/autostart  
+mkdir storage/shared/TERMUX/filesystem/autostart  
 
 # Home directory for user-specific files and settings
-mkdir storage/shared/termux/filesystem/home
+mkdir storage/shared/TERMUX/filesystem/home
 
 # Secondary hierarchy for read-only user data
-mkdir storage/shared/termux/filesystem/usr  
+mkdir storage/shared/TERMUX/filesystem/usr  
 
 # Variable data directory for files that are expected to change during normal operation
-mkdir storage/shared/termux/filesystem/var
+mkdir storage/shared/TERMUX/filesystem/var
 
 # Temporary directory for temporary files
-mkdir storage/shared/termux/filesystem/tmp
+mkdir storage/shared/TERMUX/filesystem/tmp
 
 # Directory for system-wide configuration files
-mkdir storage/shared/termux/filesystem/etc  
+mkdir storage/shared/TERMUX/filesystem/etc  
 
 # Directory for optional add-on software packages
-mkdir storage/shared/termux/filesystem/opt  
+mkdir storage/shared/TERMUX/filesystem/opt  
 
 # Data directory for services provided by the system
-mkdir storage/shared/termux/filesystem/srv  
+mkdir storage/shared/TERMUX/filesystem/srv  
 
 # Directory for bootloader files
-mkdir storage/shared/termux/filesystem/boot  
+mkdir storage/shared/TERMUX/filesystem/boot  
 
 mv ~/hello/termux/.bashrc ~/.bashrc
+
 # Installs the updates, and dependencies
-#yes | bash ~/hello/termux/package.sh
-#bash ~/hello/termux/config.sh
+#yes | bash ~/hello/TERMUX/package.sh
+#bash ~/hello/TERMUX/config.sh
 #cd ~/
+
+# on finish
+mkdir storage/shared/TERMUX/TOKEN
