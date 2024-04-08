8539bcc863a02b80917702b56ffa3e3fb9d17cc6
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 06:42:05 +0800

    pacman update

--- bin/pacman.rs
+++ bin/pacman.rs
@@ -2,7 +2,8 @@
   This is not the real pacman that access the AUR...
   This is just my own version....
 */
+use std::env;
 
 fn main(){
-  println!("Testing....");
+  println!("Not Working.....");
 }
