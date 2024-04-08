b3efb14fca993557724b9ab0632d8fbb1eaf88e6
renzaspiras <aspirasrenz@gmail.com>
2024-04-08 19:44:01 +0800

    shell testing

--- bin/shell.cpp
+++ bin/shell.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include <filesystem>
 using namespace std;
+namespace fs = std::filesystem;
 
 int main(){
   while (true)
@@ -9,13 +10,33 @@ int main(){
     string input;
 
     cout << "┌──(Testing)\n└─$ ";
-    cin >> input;
+    getline(cin, input);
 
     if(input == "exit"){
       break;
     }
 
-    //cout << input << endl << endl;
+    else if (input.empty())
+    {
+      cout << "\n";
+    }
+
+    else if(input == "clear"){
+      cout << "\033[2J\033[1;1H"; 
+    }
+
+    else{
+      string debug = "./bin";
+      string neo = "~/neo/bin";
+      
+      fs::path directory_path = debug; // Current directory
+      for (const auto& entry : fs::directory_iterator(directory_path)) {
+        // Check if it's a directory
+        if (entry.is_directory()) {
+            cout << entry.path().filename() << endl;
+        }
+      }
+    }    
   }
   
   return 0;
