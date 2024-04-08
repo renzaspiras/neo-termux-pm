d87bc05a946e91fad171b607d1ed249ed715da41
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 07:04:39 +0800

    complexity

--- bin/cm.c
+++ bin/cm.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         if (strcmp(argv[1], "update") == 0) {
             printf("Executing update command...\n");            
-            system("rm -rf ~/hello && git clone https://github.com/renzaspiras/neo-termux-archlinux.git ~/hello && bash /data/data/com.termux/files/home/update.sh neotermux");
+            system("rm -rf /data/data/com.termux/files/home/hello/ && git clone https://github.com/renzaspiras/neo-termux-archlinux.git /data/data/com.termux/files/home/hello/ && bash /data/data/com.termux/files/home/hello/update.sh neotermux");
             printf("System was updated...");               
         }
 
