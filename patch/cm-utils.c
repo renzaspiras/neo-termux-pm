8b54a52e826723aa84ef2b1905a56833d38b7fea
renzaspiras <aspirasrenz@gmail.com>
2024-04-08 13:16:03 +0800

    cm patch

--- patch/cm-utils.c
+++ patch/cm-utils.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         if (strcmp(argv[1], "update") == 0) {
             printf("Executing update command...\n");
-            system("yes | pkg upgrade && yes | pkg install git && git clone https://github.com/renzaspiras/arch-termux.git ~/hello && bash ~/hello/setup.sh");
+            system("git clone https://github.com/renzaspiras/neo-termux-archlinux.git ~/hello && bash ~/hello/setup.sh");
             printf("System was updated...");               
         }
         else if (strcmp(argv[1], "add") == 0) {
