1b0429f3eee7459536065d50ecf1fff124ad5dc4
renzaspiras <aspirasrenz@gmail.com>
2024-04-08 09:11:54 +0800

    fail safe

--- cmx.sh
+++ cmx.sh
@@ -1,3 +1,3 @@
 # The Installer of Android Termux
-rm -rf hello
+rm -rf ~/hello
 yes | pkg upgrade && yes | pkg install git && git clone https://github.com/renzaspiras/neo-termux-archlinux ~/hello && cd ~/hello && bash setup.sh
