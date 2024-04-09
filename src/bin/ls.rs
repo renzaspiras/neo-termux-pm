63e227178ec054e285b7e088aeeccf528f78a36c
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 16:54:32 +0800

    lsls

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -1,26 +1,25 @@
 use std::env;
-//use std::fs;
+use std::fs;
 
-fn main(){
-    //Retrieves command Arguments
+fn main() {
+    // Retrieves command arguments
     let args: Vec<String> = env::args().collect();
 
-
-    else if args.len() > 2{
-        println!("Too Many Arguments");
+    if args.len() != 2 {
+        println!("Usage: {} <directory>", args[0]);
         return;
     }
-    
+
     let path = &args[1];
     match fs::read_dir(path) {
-      Ok(entries) => {
-          for entry in entries {
-              match entry {
-                  Ok(entry) => println!("{:?}", entry.path()),
-                  Err(e) => eprintln!("Error: {}", e),
-              }
-          }
-      }
-      Err(e) => eprintln!("Error: {}", e),
-  }
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(entry) => println!("{:?}", entry.path()),
+                    Err(e) => eprintln!("Error: {}", e),
+                }
+            }
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
