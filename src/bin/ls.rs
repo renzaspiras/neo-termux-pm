c55c7d57697099d25b16fbd92b34151f8d0e96c3
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 16:51:46 +0800

    patched ls

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -6,10 +6,6 @@ fn main(){
     //Retrieves command Arguments
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 1 {
-        println!("No arguments provided");
-        return; // Exit the program early
-    }
     else if args.len() > 2{
         println!("Too Many Arguments");
         return;
