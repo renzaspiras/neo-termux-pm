57fa7df8938ec17e99729cdd4ca92aa405c45744
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 16:00:24 +0800

    fixed compiler

--- src/debug/compiler.py
+++ src/debug/compiler.py
@@ -8,14 +8,14 @@ try:
     extention = j.split(".")[1]
 
     if(extention == "rs"):
-      os.system(f"rustc src/bin/{j} -o ../../bin/{executable}")  
+      os.system(f"rustc src/bin/{j} -o bin/{executable}")  
     elif(extention == "cpp"):
-      os.system(f"g++ src/bin/{j} -o ../../bin/{executable}")
+      os.system(f"g++ src/bin/{j} -o bin/{executable}")
     elif(extention == "c++"):
-      os.system(f"g++ src/bin/{j} -o ../../bin/{executable} -lncurses")
+      os.system(f"g++ src/bin/{j} -o bin/{executable} -lncurses")
     elif(extention == "c"):
-      os.system(f"gcc src/bin/{j} -o ../../bin/{executable}")
+      os.system(f"gcc src/bin/{j} -o bin/{executable}")
     elif(extention == "zig"):
-      os.system(f"zig build-exe ../../bin/{j} -o bin/{executable}")      
+      os.system(f"zig build-exe src/bin/{j} -o bin/{executable}")      
 except:
   pass
