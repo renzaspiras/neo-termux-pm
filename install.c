44d048b2707882faa32ad3da3a53c3b843769818
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 16:03:08 +0800

    ziggy

--- install.c
+++ install.c
@@ -5,6 +5,9 @@ int main(){
 
   // Compiles Everyting inside the src -o ./bin
   system("mkdir bin");
+
+  // Compile the SRC
   system("python src/debug/compiler.py");
+  system("");
   return 0;
 }
