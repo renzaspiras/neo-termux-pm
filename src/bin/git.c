a7ac7f8ef2e460940f6a67cabacb2e23c67192bd
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 17:55:34 +0800

    false git

--- src/bin/git.c
+++ src/bin/git.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-  system("git");
-  return 0;
+int main(int argc, char *argv[]) {
+    // Concatenate all the arguments into a single string
+    char command[1024] = "git";
+    for (int i = 1; i < argc; i++) {
+        strcat(command, " ");
+        strcat(command, argv[i]);
+    }
+
+    // Execute the command
+    system(command);
+
+    return 0;
 }
