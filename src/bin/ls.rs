eb6ad467c5aed2556e7fdd2ee2e5400cf241237b
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 16:53:02 +0800

    ls hardened

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -1,10 +1,10 @@
 use std::env;
 use std::fs;
-use std::path::Path;
 
 fn main(){
     //Retrieves command Arguments
     let args: Vec<String> = env::args().collect();
+
 
     else if args.len() > 2{
         println!("Too Many Arguments");
@@ -12,15 +12,15 @@ fn main(){
     }
     
     let path = &args[1];
-    match fs::read_dir(Path::new(path)) {
-        Ok(entries) => {
-            for entry in entries {
-                match entry {
-                    Ok(entry) => println!("{:?}", entry.path()),
-                    Err(e) => eprintln!("Error: {}", e),
-                }
-            }
-        }
-        Err(e) => eprintln!("Error: {}", e),
-    }
+    match fs::read_dir(path) {
+      Ok(entries) => {
+          for entry in entries {
+              match entry {
+                  Ok(entry) => println!("{:?}", entry.path()),
+                  Err(e) => eprintln!("Error: {}", e),
+              }
+          }
+      }
+      Err(e) => eprintln!("Error: {}", e),
+  }
 }
