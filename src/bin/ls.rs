22be28c1ad0ee288913d9986462e1bca7725e243
renzaspiras <aspirasrenz@gmail.com>
2024-04-09 17:00:05 +0800

    no error

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -5,6 +5,11 @@ fn main() {
     // Retrieves command arguments
     let args: Vec<String> = env::args().collect();
 
+    if args.len() != 2 {
+        println!("Usage: {} <directory>", args[0]);
+        return;
+    }
+
     let path = &args[1];
     match fs::read_dir(path) {
         Ok(entries) => {
